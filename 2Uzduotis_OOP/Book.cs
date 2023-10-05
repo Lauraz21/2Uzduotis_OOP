da75982d7e924f0c049e8e327172412e74561f05
Laura <Laura@DESKTOP-BESSB6E>
2023-10-05 22:05:42 +0300

    Add project files.

---
+++ 2Uzduotis_OOP/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Uzduotis_OOP
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Years { get; set; }
+        public string Country { get; set; }
+
+
+        public Book(string name, string author, int years, string country)
+        {
+            Name = name;
+            Author = author;
+            Years = years;
+            Country = country;
+        }
+    }
+}
---
+++ 2Uzduotis_OOP/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _2Uzduotis_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> allBooks = new List<Book>();
+
+            Book book1 = new Book("Harry Potter", "J.K.Rowling", 2007, "UK");
+            allBooks.Add(book1);
+
+            Book book2 = new Book("knyga", "Laura", 2005, "UK");
+            allBooks.Add(book2);
+
+            Book book3 = new Book("Harry Potter2", "J.K.Rowling", 2004, "UK");
+            allBooks.Add(book3);
+
+
+
+
+            List<Book> filterBooks = FindAuthorBooks(allBooks, "J.K.Rowling");
+
+            foreach (Book book in filterBooks)
+            {
+                Console.WriteLine($"{book.Name} {book.Author} {book.Years} {book.Country}");
+            }
+
+
+        }
+        public static List<Book> FindAuthorBooks(List<Book> books, string author)
+        {
+            List<Book> newBooks = new List<Book>();
+
+            foreach (Book book in books)
+            {
+                if (book.Author == author)
+                {
+                    newBooks.Add(book);
+                }
+            }
+            return newBooks;
+        }
+    }
+}
